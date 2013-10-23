ce952a6ed094834cf3dc0565e2dd3571ac6082a0
Avery <Sawyer@Averys-MacBook-Air.local>
2013-10-23 10:58:06 -0700

    most of things

--- js/script.js
+++ js/script.js
@@ -18,6 +18,27 @@ $(document).ready(function(){
 		updateNav($(this).attr('href').substring(1));
 		return true;
 	});
+	
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				m4v: "http://handbasketproductions.com/DIAv1_Full_Trailer.mp4"
+			});
+		},
+		swfPath: "js/libs/",
+		supplied: "m4v",
+		size: {
+			width: "600px",
+			height: "360px",
+			cssClass: "jp-video-360p"
+		},
+		smoothPlayBar: true,
+		keyEnabled: true,
+		errorAlerts:true,
+		wmode:"window",
+		preload:'auto'
+	});
+	
 	updateNav(hash);
 });
 
